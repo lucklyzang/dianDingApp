f8487d3e79b1958314ee0009bd7c571d0049af84
lucklyYong <luckyzangvip@163.com>
2022-07-27 13:45:20 +0800

    修改接口地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,12 +5,9 @@ import Vue from 'vue';
 import { Dialog, Toast } from 'vant';
 // 全局注册
 Vue.use(Dialog);
-// http://39.100.74.225/nft 测试地址本地
-// http://show.blinktech.cn/nft 测试地址线上
-// http://47.99.237.183/nft 测试地址线上
-// http://124.71.228.189/nft 正式地址
+// http://diandingshucang.com/nft 测试地址
 const service = axios.create({
-    baseURL: 'http://39.100.74.225/nft', //接口基础地址
+    baseURL: 'http://diandingshucang.com/nft', //接口基础地址
     retry: 2, // 网络请求异常后，重试次数
     retryDelay: 1000, // 每次重试间隔时间
     shouldRetry: (err) => true // 重试条件
